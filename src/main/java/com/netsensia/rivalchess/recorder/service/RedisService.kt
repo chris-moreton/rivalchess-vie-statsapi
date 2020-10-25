6ba3d70db50c4453d1dd65f04dcc1ae10287a800
Chris Moreton <chris@netsensia.com>
2020-10-25 14:24:26 +0000

    Mock Redis server for local

--- src/main/java/com/netsensia/rivalchess/recorder/service/RedisService.kt
+++ src/main/java/com/netsensia/rivalchess/recorder/service/RedisService.kt
@@ -1,5 +1,6 @@
 package com.netsensia.rivalchess.recorder.service
 
+import com.github.fppt.jedismock.RedisServer
 import com.google.gson.Gson
 import com.netsensia.rivalchess.recorder.model.RivalStatistics
 import org.springframework.stereotype.Component
@@ -11,16 +12,30 @@ class RedisService {
     private val url = System.getenv("REDIS_HOST")
     private val port = System.getenv("REDIS_PORT").toInt()
 
-    val jedis: Jedis = Jedis(url, port)
+    var jedis: Jedis? = null
+
     val gson = Gson()
+
+    fun getCacheService(): Jedis {
+        if (jedis == null) {
+            if (url.equals("mock")) {
+                val server = RedisServer.newRedisServer();  // bind to a random port
+                server.start();
+                jedis = Jedis(server.host, server.bindPort)
+            } else {
+                jedis = Jedis(url, port)
+            }
+        }
+        return jedis!!
+    }
 
     fun set(key: String, value: Any) {
         val str = gson.toJson(value)
-        jedis.set(key, str)
+        getCacheService().set(key, str)
     }
 
     fun getRivalStatistics(): RivalStatistics {
-        val json = jedis.get("RivalStatistics")
+        val json = getCacheService().get("rivalStatistics")
         return gson.fromJson(json, RivalStatistics::class.java)
     }
 
